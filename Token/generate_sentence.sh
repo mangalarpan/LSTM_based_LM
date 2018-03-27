4ead6393eac1384ebc47174b182bdc44e83d537a
mangalarpan <mangalarpan9@gmail.com>
2018-03-27 23:06:27 +0530

    modified

--- Token/generate_sentence.sh
+++ Token/generate_sentence.sh
@@ -1,2 +1,3 @@
+export PATH="/home1/e1-246-35/anaconda3/bin/:$PATH"
 cd /home1/e1-246-35/LSTM/LSTM_based_LM/Token/
 python /home1/e1-246-35/LSTM/LSTM_based_LM/Token/generate_sen_token.py
--- character/generate_sentence.sh
+++ character/generate_sentence.sh
@@ -1,2 +1,3 @@
+export PATH="/home1/e1-246-35/anaconda3/bin/:$PATH"
 cd /home1/e1-246-35/LSTM/LSTM_based_LM/character/
 python /home1/e1-246-35/LSTM/LSTM_based_LM/character/generate_sen_char.py
