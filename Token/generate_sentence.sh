20ee4154c727927e578a8efc5bb655107cd3101a
mangalarpan <mangalarpan9@gmail.com>
2018-03-27 15:29:15 +0530

    done

---
+++ Token/generate_sentence.sh
@@ -0,0 +1,2 @@
+cd /home1/e1-246-35/LSTM/LSTM_based_LM/Token/
+python /home1/e1-246-35/LSTM/LSTM_based_LM/Token/generate_sen_token.py
---
+++ character/generate_sentence.sh
@@ -0,0 +1,2 @@
+cd /home1/e1-246-35/LSTM/LSTM_based_LM/character/
+python /home1/e1-246-35/LSTM/LSTM_based_LM/character/generate_sen_char.py
